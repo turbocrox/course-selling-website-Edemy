a949f631450c52d7d03e88245cacd4eae678759c
turbocrox <adityapundir502@gmail.com>
2025-05-29 11:50:08 +0530

    asd

---
+++ src/pages/edu/Studentsenrolled.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Studentsenrolled = () => {
+  return (
+    <div>Studentsenrolled</div>
+  )
+}
+
+export default Studentsenrolled
---
+++ src/pages/stu/Myenroll.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Myenroll = () => {
+  return (
+    <div>Myenroll</div>
+  )
+}
+
+export default Myenroll
