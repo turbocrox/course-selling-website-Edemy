e83d50768b14366ef75e74c4438804e08259c817
turbocrox <adityapundir502@gmail.com>
2025-05-29 11:46:01 +0530

    asd

--- src/pages/stu/Coursedetail.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Coursedetail = () => {
-  return (
-    <div>Coursedetail</div>
-  )
-}
-
-export default Coursedetail
--- src/pages/stu/Myenroll.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Myenroll = () => {
-  return (
-    <div>Myenroll</div>
-  )
-}
-
-export default Myenroll
