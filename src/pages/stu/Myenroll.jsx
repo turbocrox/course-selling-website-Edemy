4bcb6ce5dd4ad78b52c12112b8551cb50edba93f
turbocrox <adityapundir502@gmail.com>
2025-07-22 11:33:16 +0530

    a

--- src/pages/stu/Myenroll.jsx
+++ src/pages/stu/Myenroll.jsx
@@ -2,7 +2,20 @@ import React from 'react'
 
 const Myenroll = () => {
   return (
-    <div></div>
+   <>
+   <div className='md:px-36 px-8 pt-10'>
+      <h1  className='text-2xl font-semibold'>My Enrollments </h1>
+      <table  className='md:table-auto  table-fixed  w-full  overflow-hidden border  mt-10 '>
+        <tr>
+          <th className='px-4 py-3 font-semibold truncate'>Course</th>
+          <th className='px-4 py-3 font-semibold truncate'>Duration</th>
+          <th className='px-4 py-3 font-semibold truncate'>Completed</th>
+          <th className='px-4 py-3 font-semibold truncate'>Status</th>
+
+        </tr>
+      </table>
+    </div>
+    </>
   )
 }
 
